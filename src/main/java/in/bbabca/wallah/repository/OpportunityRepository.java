d54f7b8101c4aa35d8a0433f54bd5695ab023921
Sourav Kumbhakar <108406599+Sourav12kumar@users.noreply.github.com>
2026-09-07 22:21:20 +0530

    feat: add opportunity repository

---
+++ src/main/java/in/bbabca/wallah/repository/OpportunityRepository.java
@@ -0,0 +1,13 @@
+package in.bbabca.wallah.repository;
+
+import in.bbabca.wallah.model.Opportunity;
+import in.bbabca.wallah.model.OpportunityType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OpportunityRepository extends JpaRepository<Opportunity, Long> {
+    List<Opportunity> findByTypeAndActiveTrueOrderByFeaturedDescDeadlineAscCreatedAtDesc(OpportunityType type);
+    List<Opportunity> findTop6ByActiveTrueOrderByFeaturedDescCreatedAtDesc();
+    long countByType(OpportunityType type);
+}
