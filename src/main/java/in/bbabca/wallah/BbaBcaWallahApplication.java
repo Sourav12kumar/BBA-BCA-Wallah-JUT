43e79a370c452412f0ea816503556f86a4aaef21
Sourav Kumbhakar <108406599+Sourav12kumar@users.noreply.github.com>
2026-09-07 18:57:00 +0530

    feat: add Spring Boot application entry point

---
+++ src/main/java/in/bbabca/wallah/BbaBcaWallahApplication.java
@@ -0,0 +1,11 @@
+package in.bbabca.wallah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbaBcaWallahApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BbaBcaWallahApplication.class, args);
+    }
+}
