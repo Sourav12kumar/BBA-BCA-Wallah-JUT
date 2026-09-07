f44c0b6f11c70b615d262a5a4bdd74cae053e84b
Sourav Kumbhakar <108406599+Sourav12kumar@users.noreply.github.com>
2026-09-07 19:06:46 +0530

    feat: serve uploaded academic resources

---
+++ src/main/java/in/bbabca/wallah/controller/FileController.java
@@ -0,0 +1,28 @@
+package in.bbabca.wallah.controller;
+
+import in.bbabca.wallah.service.FileStorageService;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class FileController {
+
+    private final FileStorageService fileStorageService;
+
+    public FileController(FileStorageService fileStorageService) {
+        this.fileStorageService = fileStorageService;
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    public ResponseEntity<Resource> download(@PathVariable String filename) {
+        Resource resource = fileStorageService.load(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+}
